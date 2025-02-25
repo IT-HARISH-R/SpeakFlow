51eb8f516b2b6eb9bdeade5e03364298179b260e
IT-HARISH-R <harishmass27.8@gmail.com>
2025-02-25 11:40:14 +0530

    setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,19 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 
-import path from "path";
+const path = require("path");
 
-  
+
 const app = express();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({ 
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
 );
 
 
@@ -24,12 +24,12 @@ console.log("hello")
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
 
-  
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+
 module.exports = app;
 
